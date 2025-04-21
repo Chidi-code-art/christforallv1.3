830fc3923617b5cc6f0fb5d54cbd798486d5ddb0
Chidi-code-art <jasonprogrammer63@gmail.com>
2025-04-21 18:33:14 +0100

    fourth commit

--- jesusweb/jesusweb/settings.py
+++ jesusweb/jesusweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-qq6p4=@z36oiw%4qbkwqv5zmle
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True')== 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', "127.0.0.1,localhost").split(',')
 
 
 # Application definition
