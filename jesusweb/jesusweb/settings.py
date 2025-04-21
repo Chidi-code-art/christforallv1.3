43988065a1182c8ec1880495dac7ca9567be6933
Chidi-code-art <jasonprogrammer63@gmail.com>
2025-04-21 17:57:33 +0100

    second-commit

--- jesusweb/jesusweb/settings.py
+++ jesusweb/jesusweb/settings.py
@@ -19,11 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qq6p4=@z36oiw%4qbkwqv5zmle62f^*prhyiwva)sc40b6vvl1'
-
+# SECURITY WARNING: keep the secret key used in production secret          
+#SECRET_KEY= 'django-insecure-qq6p4=@z36oiw%4qbkwqv5zmle62f^*prhyiwva)sc40b6vvl1'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-qq6p4=@z36oiw%4qbkwqv5zmle62f^*prhyiwva)sc40b6vvl1')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True')== 'True'
 
 ALLOWED_HOSTS = []
 
